5f3152450f0ac861f5ee0fe511f3b91d10e4ea02
Rabah Ali Shah <muhammadrabah.ali46@gmail.com>
2023-08-22 14:38:44 +0500

    fixed warnings

--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { FcLike } from "react-icons/fc";
 import { Link } from "react-router-dom";
 import axios from "axios";
